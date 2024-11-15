3514f537c374ba9061beb055a82f8e6cd33a1c41
Thiago <thiago2b9s@gmail.com>
2024-11-15 17:48:36 -0300

    aaaaaaaaaaaaaaaaaa

--- backend/internal/app.go
+++ backend/internal/app.go
@@ -52,7 +52,12 @@ func (a *App) Init() {
 
 func (a *App) setupHttp() {
 	app := fiber.New()
-	app.Use(cors.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://127.0.0.1:5173",
+		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+		AllowCredentials: true,
+}))
 
 	quizController := controller.Quiz(a.quizService)
 	userController := controller.User(a.userService)
